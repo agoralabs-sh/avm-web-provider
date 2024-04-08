bb57fbfc8383b21726526e12062a293e6da62825
Kieran O'Neill <hello@kieranoneill.com>
2024-04-08 13:33:57 +0100

    chore: squash

--- src/types/TRequestParams.ts
+++ src/types/TRequestParams.ts
@@ -3,11 +3,13 @@ import type IDisableParams from './IDisableParams';
 import type IDiscoverParams from './IDiscoverParams';
 import type IEnableParams from './IEnableParams';
 import type IPostTransactionsParams from './IPostTransactionsParams';
+import type ISignTransactionsParams from './ISignTransactionsParams';
 
 type TRequestParams =
   | IDisableParams
   | IDiscoverParams
   | IEnableParams
-  | IPostTransactionsParams;
+  | IPostTransactionsParams
+  | ISignTransactionsParams;
 
 export default TRequestParams;
