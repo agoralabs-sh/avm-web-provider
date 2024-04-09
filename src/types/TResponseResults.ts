7bed5bec6f6d58932b14ffc87f364ac93882ee35
Kieran O'Neill <hello@kieranoneill.com>
2024-04-09 08:23:10 +0100

    feat: add types for sign transactions method

--- src/types/TResponseResults.ts
+++ src/types/TResponseResults.ts
@@ -3,11 +3,13 @@ import type IDisableResult from './IDisableResult';
 import type IDiscoverResult from './IDiscoverResult';
 import type IEnableResult from './IEnableResult';
 import type IPostTransactionsResult from './IPostTransactionsResult';
+import type ISignTransactionsResult from './ISignTransactionsResult';
 
 type TResponseResults =
   | IDisableResult
   | IDiscoverResult
   | IEnableResult
-  | IPostTransactionsResult;
+  | IPostTransactionsResult
+  | ISignTransactionsResult;
 
 export default TResponseResults;
