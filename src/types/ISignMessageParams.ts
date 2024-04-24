0bdf773e0fccea7d8f92720307e912b84ced6647
Kieran O'Neill <hello@kieranoneill.com>
2024-04-24 18:54:34 +0100

    feat: make signer optional in sign message method

--- src/types/ISignMessageParams.ts
+++ src/types/ISignMessageParams.ts
@@ -1,12 +1,12 @@
 /**
  * @property {string} message - a human-readable UTF-8 string to sign.
  * @property {string} providerId - [optional] a unique identifier for the provider.
- * @property {string} signer - the address to be used to sign the message.
+ * @property {string} signer - [optional] the address to be used to sign the message.
  */
 interface ISignMessageParams {
   message: string;
   providerId?: string;
-  signer: string;
+  signer?: string;
 }
 
 export default ISignMessageParams;
--- src/types/ISignMessageResult.ts
+++ src/types/ISignMessageResult.ts
@@ -2,10 +2,12 @@
  * @property {string} providerId - a unique identifier for the provider.
  * @property {string} signature - a base64 encoded signature of the message signed by the private key of the intended
  * signer.
+ * @property {string} signer - the address used to sign the message.
  */
 interface ISignMessageResult {
   providerId: string;
   signature: string;
+  signer: string;
 }
 
 export default ISignMessageResult;
