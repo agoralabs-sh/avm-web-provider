9fa65dfb9dbeb24937e6b3ee621537d801f76d13
Kieran O'Neill <hello@kieranoneill.com>
2025-02-18 13:27:30 +0000

    feat: add utility function to sign typed data

---
+++ src/enums/ARC0060ErrorTypeEnum.ts
@@ -0,0 +1,6 @@
+enum ARC0060ErrorTypeEnum {
+  FailedDomainAuthError = 'ERROR_FAILED_DOMAIN_AUTH',
+  InvalidScopeError = 'ERROR_INVALID_SCOPE',
+}
+
+export default ARC0060ErrorTypeEnum;
---
+++ src/errors/arc0060/index.ts
@@ -0,0 +1,3 @@
+export { default as ARC0060FailedDomainAuthError } from './ARC0060FailedDomainAuthError';
+export { default as ARC0060InvalidScopeError } from './ARC0060InvalidScopeError';
+export { default as BaseARC0060Error } from './BaseARC0060Error';
